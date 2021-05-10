e412c91c2b6ee45c33f7ec66bda5190a22e6b5a7
lotarev <foermanDT@gmail.com>
2021-05-10 14:17:23 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
-import './App.css';
-import React, {ReactElement, useEffect} from "react";
-import {compose} from "redux";
-import { withRouter, RouteComponentProps} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {StateType} from './redux/state';
-import {getPostsData, ThunkPostsType} from './redux/postsRedusers';
-import {getCommentsPost, getPostOpen, ThunkPostType} from "./redux/postRedusers";
-import Posts from './Posts/Posts';
+import "./App.css";
+import React, { ReactElement, useEffect } from "react";
+import { compose } from "redux";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import { connect } from "react-redux";
+import { StateType } from "./redux/state";
+import { getPostsData, ThunkPostsType } from "./redux/postsRedusers";
+import {
+  getCommentsPost,
+  getPostOpen,
+  ThunkPostType,
+} from "./redux/postRedusers";
+import Posts from "./Posts/Posts";
 
 type MdtpType = {
-    getPostsData: () => ThunkPostsType
-    getCommentsPost: (postId:number) => ThunkPostType
-    getPostOpen: (postId:number) => ThunkPostType
-}
-const App: React.FC<MdtpType> = (props):ReactElement<any,any> => {
+  getPostsData: () => ThunkPostsType;
+  getCommentsPost: (postId: number) => ThunkPostType;
+  getPostOpen: (postId: number) => ThunkPostType;
+};
+const App: React.FC<MdtpType> = (props): ReactElement<any, any> => {
+  useEffect(() => {
+    props.getPostsData();
+    props.getCommentsPost(1);
+    props.getPostOpen(1);
+    // props.setMessageError('ерунда какая то')
+  }, []);
 
-    useEffect(()=> {
-        props.getPostsData()
-        props.getCommentsPost(1)
-        props.getPostOpen(1)
-        // props.setMessageError('ерунда какая то')
-    }, [] )
-
-    return (<>
-        <Posts/>
-        </>)
-}
-console.log(App)
-const AppContainer = compose<MdtpType & RouteComponentProps> (
-    withRouter,
-    connect<null, MdtpType,null,StateType >(null, {getPostsData,getCommentsPost, getPostOpen}),
+  return (
+    <>
+      <Posts />
+    </>
+  );
+};
+console.log(App);
+const AppContainer = compose<MdtpType & RouteComponentProps>(
+  withRouter,
+  connect<null, MdtpType, null, StateType>(null, {
+    getPostsData,
+    getCommentsPost,
+    getPostOpen,
+  })
 )(App);
-console.log(AppContainer)
+console.log(AppContainer);
 
-export default <AppContainer/>;
+export default <AppContainer />;
